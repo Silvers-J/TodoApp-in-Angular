23e1f2efcda9e83282163f80a0939e0db8b840f7
Jayesh M <mandlik87@gmail.com>
2020-09-02 15:18:23 +0530

    Added status, item and newTask component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusComponent } from './components/status/status.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    TodoItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
