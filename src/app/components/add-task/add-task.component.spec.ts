3dfbeb77923b63dfdee0e816ae31a42bb5ac3b0e
Jayesh M <mandlik87@gmail.com>
2021-02-21 15:07:22 +0530

    Theme changes

--- src/app/components/add-task/add-task.component.spec.ts
+++ src/app/components/add-task/add-task.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddTaskComponent } from './add-task.component';
+import {StorageService} from 'src/app/services/storage.service';
+import {Status} from 'src/app/model/Status';
 
 describe('AddTaskComponent', () => {
   let component: AddTaskComponent;
-  let fixture: ComponentFixture<AddTaskComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddTaskComponent ]
-    })
-    .compileComponents();
-  }));
+  let storage: jasmine.SpyObj<StorageService>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddTaskComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    storage = jasmine.createSpyObj('StorageService', ['storeTask', 'getTasks']);
+    component = new AddTaskComponent(storage);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('task should have been initialized', () => {
+    component.initPerson();
+    expect(component.task.title).toEqual('');
+    expect(component.task.description).toEqual('');
+    expect(component.task.status).toEqual(Status.pending);
+  });
+
+  it('task should get unique id', () => {
+    let task = [
+      {
+        id: 1,
+        title: 'task 1',
+        description: 'task 1',
+        status: Status.pending
+      },
+      {
+        id: 2,
+        title: 'task 2',
+        description: 'task 2',
+        status: Status.completed
+      }
+    ];
+    storage.getTasks.and.returnValue(task);
+    component.initPerson();
+    expect(component.isUniqueID(component.task.id)).toBeTrue();
   });
 });
